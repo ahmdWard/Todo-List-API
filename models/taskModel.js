59d6ca52447b144a206439c0f6a9986830a0484a
ahmdWard <ahmdemadabdallah@gmail.com>
2024-09-06 12:26:27 +0300

    calc time of each task

--- models/taskModel.js
+++ models/taskModel.js
@@ -12,8 +12,34 @@ const taskSchema= new mongoose.Schema({
         type:String,
         enum:['done','in progress'],
         default:"in progress"
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+    timeLasted:{
+        type:String
     }
 })
 // calc the time of each task in min 
+taskSchema.pre('findOneAndUpdate', async function (next) {
+    const update = this.getUpdate();
+    const docToUpdate = await this.model.findOne(this.getQuery());
+    const date = parseInt((Date.now() - docToUpdate.createdAt) / 1000 / 60, 10);
+
+    if (update.status === 'done' && docToUpdate.status !== 'done') {
+        update.timeLasted = `${date} min `;
+    } 
+    else if (update.status === 'in progress') {
+        update.$unset = update.$unset || {};
+        update.$unset.timeLasted = "";
+    }
+
+    this.setUpdate(update);
+    next();
+});
+
+
+
 
 module.exports=mongoose.model('Task',taskSchema)
